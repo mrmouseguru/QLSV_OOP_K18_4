d39225837e722b3d6176553553d65f189a408ef6
CGM <mrmouse.guru@gmail.com>
2025-04-03 14:26:38 +0700

    NGOẠI LỆ - EXCEPTION

---
+++ src/exception/DemoException.java
@@ -0,0 +1,25 @@
+package exception;
+
+public class DemoException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int result = 0;
+		
+		try {
+			result = a / 0;
+		}
+		
+		catch(ArithmeticException arithmeticException) {
+			System.out.println("Lỗi chia cho 0"
+					+ arithmeticException.getMessage());
+			
+		}
+		
+		
+		System.out.println("Result: " + result);
+
+	}
+
+}
